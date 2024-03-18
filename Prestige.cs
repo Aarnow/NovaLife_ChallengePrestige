ccc7f2d054df4ca531d6d799151f9452dc5bece8
Aarnow <arnaudfgr.dev@gmail.com>
2024-03-18 11:05:28 +0100

    Initial Project Files

---
+++ Prestige.cs
@@ -0,0 +1,25 @@
+﻿using Life;
+using ModKit.Helper;
+using ModKit.Interfaces;
+using ModKit.Internal;
+
+
+namespace ChallengePrestige
+{
+    public class Prestige : ModKit.ModKit
+    {
+
+        public Prestige(IGameAPI api) : base(api)
+        {
+            PluginInformations = new PluginInformations(AssemblyHelper.GetName(), "1.0.0", "Aarnow");
+        }
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+
+
+            Logger.LogSuccess($"{PluginInformations.SourceName} v{PluginInformations.Version}", "initialisé");
+        }
+    }
+}
